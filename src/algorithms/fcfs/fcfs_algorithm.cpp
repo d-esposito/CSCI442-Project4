d3308baff173e3800c4282bef86a78d2e8f723d7
David Esposito <davidesposito@mines.edu>
2021-10-25 12:39:21 -0600

    Wrote FCFS, but still getting the seg fault

--- src/algorithms/fcfs/fcfs_algorithm.cpp
+++ src/algorithms/fcfs/fcfs_algorithm.cpp
@@ -18,14 +18,22 @@ FCFSScheduler::FCFSScheduler(int slice) {
 
 std::shared_ptr<SchedulingDecision> FCFSScheduler::get_next_thread() {
         // TODO: implement me!
-        return nullptr;
+        std::shared_ptr<SchedulingDecision> decision = std::make_shared<SchedulingDecision>();
+
+        decision->thread = ready_queue.front();
+        decision->explanation = "Selected from " + std::to_string(ready_queue.size()) + " threads. Will run to completion of burst.";
+        decision->time_slice = -1;
+        ready_queue.pop();
+
+        return decision;
 }
 
 void FCFSScheduler::add_to_ready_queue(std::shared_ptr<Thread> thread) {
         // TODO: implement me!
+        ready_queue.push(thread);
 }
 
 size_t FCFSScheduler::size() const {
         // TODO: implement me!
-        return 0;
+        return ready_queue.size();
 }
--- src/algorithms/fcfs/fcfs_algorithm.hpp
+++ src/algorithms/fcfs/fcfs_algorithm.hpp
@@ -22,6 +22,7 @@ public:
     //==================================================
 
     // TODO: Add any member variables you may need.
+    std::queue<std::shared_ptr<Thread>> ready_queue;
 
     //==================================================
     //  Member functions
