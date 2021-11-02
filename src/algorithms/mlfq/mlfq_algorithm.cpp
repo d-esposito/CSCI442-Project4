589554dfff418c1c4b5a6ea7461c745e06f1e0e9
David Esposito <davidesposito@mines.edu>
2021-11-02 11:58:05 -0600

    Added mlfq class files

---
+++ src/algorithms/mlfq/mlfq_algorithm.cpp
@@ -0,0 +1,38 @@
+#include "algorithms/mlfq/mlfq_algorithm.hpp"
+
+#include <cassert>
+#include <stdexcept>
+
+#define FMT_HEADER_ONLY
+#include "utilities/fmt/format.h"
+
+MLFQScheduler::MLFQScheduler(int slice) {
+    if (slice <= 0) {
+        time_slice = 3;
+    }
+    else {
+        time_slice = slice;
+    }
+}
+
+std::shared_ptr<SchedulingDecision> MLFQScheduler::get_next_thread() {
+        std::shared_ptr<SchedulingDecision> decision = std::make_shared<SchedulingDecision>();
+        if (!ready_queue.empty()) {
+            decision->thread = ready_queue.front();
+            decision->explanation = "Selected from " + std::to_string(ready_queue.size()) + " threads. Will run for at most " + std::to_string(time_slice) + " ticks.";
+            ready_queue.pop();
+        } else {
+            decision->explanation = "No threads available for scheduling.";
+        }
+        decision->time_slice = time_slice;
+
+        return decision;
+}
+
+void MLFQScheduler::add_to_ready_queue(std::shared_ptr<Thread> thread) {
+        ready_queue.push(thread);
+}
+
+size_t MLFQScheduler::size() const {
+        return ready_queue.size();
+}
